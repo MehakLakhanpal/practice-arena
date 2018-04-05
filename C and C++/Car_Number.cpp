fbdf07ccaed6ac136a0ee2df9f55ade3a5d294e4
Mehak <mehaklakhanpal1332@gmail.com>
2018-04-05 23:46:31 +0530

    changed approach of solving the problem.

--- C and C++/Car_Number.cpp
+++ C and C++/Car_Number.cpp
@@ -8,11 +8,13 @@ int main()
 	{
 		cin>>Number;
 		int sum=0;
-		while(Number!=0)
-		{
-			sum +=Number%10;
-			Number/=10;
-		}
+//		while(Number!=0)
+//		{
+//			sum +=Number%10;
+//			Number/=10;
+//		}
+		// Because it is given that a car number would be 4 digit.
+		sum = Number%10 + Number%100 + Number%1000 + Number/1000; 
 		if(sum%2==0)
 		{
 			if(sum%4==0)
