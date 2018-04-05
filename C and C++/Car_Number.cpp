65236c41bfbd0ff4620d0557cc0295de670e9d38
Sukhsagar <sukhsagar.batra@gmail.com>
2018-04-06 00:36:02 +0530

    fixed bugs.

--- C and C++/Car_Number.cpp
+++ C and C++/Car_Number.cpp
@@ -2,34 +2,34 @@
 using namespace std;
 int main()
 {
-	int N,Number;
-	cin>>N;
-	while(N--)
+	int n;
+	cin>>n;
+	int number[n];
+	int sum[n];
+	int counter=0;
+	while(counter!=n)
 	{
-		cin>>Number;
-		int sum=0;
-//		while(Number!=0)
-//		{
-//			sum +=Number%10;
-//			Number/=10;
-//		}
-		// Because it is given that a car number would be 4 digit.
-		sum = Number%10 + Number%100 + Number%1000 + Number/1000; 
-		if(sum%2==0)
+		cin>>number[counter];
+		while(number[counter]!=0)
 		{
-			if(sum%4==0)
-			  cout<<"Yes"<<endl;
-			else
-			  cout<<"No"<<endl;
-			
+			sum[counter] +=number[counter]%10;
+			number[counter]/=10;
 		}
+		counter++;
+	}
+	
+	//Reset counter.
+	counter=0;
+	while(counter!=n){
+		if(sum[counter]%3==0)
+			cout<<"Yes"<<endl;
+		else if (sum[counter]%4==0){
+			if(sum[counter]%2==0)
+				cout<<"No"<<endl;	
+		}			
 		else
-		{
-			if(sum%3==0)
-			  cout<<"Yes"<<endl;
-			else
-			  cout<<"No"<<endl;
-		}
+			cout<<"No"<<endl;
+		counter++;
 	}
 	return 0;
 }
